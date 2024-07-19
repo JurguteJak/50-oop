5fabdbc3cd7f5449f9f6c6be7f3368c05613beab
JurguteJak <jurgutejak@gmail.com>
2024-07-19 14:00:48 +0300

    dog think

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import { Dog } from "./js/Dog.js";
 
 const rexas = new Dog('Rexas', 'brown', 3);
 console.log(rexas);
-console.log(rexas.name);
-console.log(rexas.furColor);
-console.log(rexas.age);
 
+console.log(rexas.hi());
+console.log(rexas.think());
+console.log(rexas.addNumbers(5, 1));
+console.log(rexas.addNumbers(3, -2));
+console.log(rexas.addNumbers(-3, -2));
+console.log(rexas.addNumbers(-3, 3));
+console.log(rexas.manyBones(20));
 
--- js/Dog.js
+++ js/Dog.js
@@ -4,4 +4,25 @@ export class Dog {
         this.furColor = kailioSpalva;
         this.age = amzius;
     }
+
+    hi() {
+        return 'Suo sako: au au!!';
+    }
+    think() {
+        return 'Suo galvoja: 🦴🦴🦴';
+    }
+    addNumbers(a, b) {
+        if (a + b < 0) {
+            return 'Suo yra liudnas...negali suskaiciuoti negatyvaus kaulu kiekio'
+        }
+
+        if (a + b === 0) {
+            return 'Suns reakcija❓❓❓'
+        }
+
+        return `Suo suskaiciavo: ${a} + ${b} = ${'🦴'.repeat(a + b)}.`;
+    }
+    manyBones(count) {
+        return `Many bones: ${'🦴'.repeat(count)}`;
+    }
 }
